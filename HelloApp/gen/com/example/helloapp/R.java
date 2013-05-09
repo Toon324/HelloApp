186022598186caafac48ac8b1cf84e7ee9a6ca00
Cody Swendrowski <cswendrowski@mail.com>
2013-05-08 21:57:12 -0500

    Adapts HelloApp to start becoming the CEA demo app.

--- HelloApp/gen/com/example/helloapp/R.java
+++ HelloApp/gen/com/example/helloapp/R.java
@@ -25,7 +25,7 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
-        public static final int edit_message=0x7f080001;
+        public static final int peopleList=0x7f080001;
         public static final int section_label=0x7f080002;
         public static final int textView1=0x7f080000;
     }
--- HelloApp/src/com/example/helloapp/MainActivity.java
+++ HelloApp/src/com/example/helloapp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.helloapp;
 
-import android.content.Intent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
 
 	public final static String EXTRA_MESSAGE = "com.example.helloapp.MESSAGE";
 
@@ -20,15 +26,61 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ListView listview = (ListView) findViewById(R.id.peopleList);
+		String[] values = new String[] { "Cody Swendrowski", "Forrest Galepetko", "Casey Rogers", "Guest" };
 
+		final ArrayList<String> list = new ArrayList<String>();
+		for (int i = 0; i < values.length; ++i) {
+			list.add(values[i]);
+		}
+		final StableArrayAdapter adapter = new StableArrayAdapter(this,
+				android.R.layout.simple_list_item_1, list);
+		listview.setAdapter(adapter);
+
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, final View view,
+					int position, long id) {
+				final String item = (String) parent.getItemAtPosition(position);
+				
+				view.animate().setDuration(2000).alpha(0)
+						.withEndAction(new Runnable() {
+							@Override
+							public void run() {
+								list.remove(item);
+								adapter.notifyDataSetChanged();
+								view.setAlpha(1);
+							}
+						});
+			}
+
+		});
 	}
 
-	/** Called when the user clicks the Send button */
-	public void sendMessage(View view) {
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
+}
+
+class StableArrayAdapter extends ArrayAdapter<String> {
+
+	HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+	public StableArrayAdapter(Context context, int textViewResourceId,
+			List<String> objects) {
+		super(context, textViewResourceId, objects);
+		for (int i = 0; i < objects.size(); ++i) {
+			mIdMap.put(objects.get(i), i);
+		}
 	}
+
+	@Override
+	public long getItemId(int position) {
+		String item = getItem(position);
+		return mIdMap.get(item);
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+
 }
